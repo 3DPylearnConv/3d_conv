216c92e0d3f506309ff9aa8fff0696e8cfa71996
Joaquín Ruales <joaqo182@gmail.com>
2015-03-26 15:00:47 -0400

    Added downscale3d layer type

--- 3d_conv/layers/layer_utils.py
+++ 3d_conv/layers/layer_utils.py
@@ -10,3 +10,15 @@ def dropout(rng, values, p):
     mask = srng.binomial(n=1, p=p, size=values.shape, dtype=theano.config.floatX)
     output =  values * mask
     return  numpy.cast[theano.config.floatX](1.0/p) * output
+
+def downscale3d(the_3d_array, downscale_factor):
+    array_shape = the_3d_array.shape
+    return np.round(the_3d_array.reshape(array_shape[0], \
+                                         array_shape[1]/downscale_factor, \
+                                         downscale_factor, \
+                                         array_shape[2], \
+                                         array_shape[3]/downscale_factor, \
+                                         downscale_factor, \
+                                         array_shape[4]/downscale_factor, \
+                                         downscale_factor) \
+                                 .mean(axis=(2, 5, 7)))
