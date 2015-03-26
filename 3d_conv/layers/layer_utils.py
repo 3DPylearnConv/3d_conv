8c21778bb6d7fb6da07c43c66cf039b12028eaa5
Joaquín Ruales <joaqo182@gmail.com>
2015-03-26 15:42:27 -0400

    Added 3d min/max pooling function using numpy

--- 3d_conv/layers/layer_utils.py
+++ 3d_conv/layers/layer_utils.py
@@ -15,7 +15,7 @@ def dropout(rng, values, p):
     return numpy.cast[theano.config.floatX](1.0 / p) * output
 
 
-def downscale3d(the_5d_input, downscale_factor):
+def downscale_3d(the_5d_input, downscale_factor):
     """
     Downscales a 3d layer (represented as a 5d BZCXY array) by the same downscale_factor in each dimension. Assumes that each of
     the 3 spatial dimensions has size divisible by the downscale factor.
@@ -30,3 +30,37 @@ def downscale3d(the_5d_input, downscale_factor):
                                             array_shape[4] / downscale_factor,
                                             downscale_factor)
                        .mean(axis=(2, 5, 7)))
+
+
+def min_pool_3d(the_5d_input, downscale_factor):
+    """
+    Min-pools a 3d layer (represented as a 5d BZCXY array) by the same downscale_factor in each dimension. Assumes that each of
+    the 3 spatial dimensions has size divisible by the downscale factor.
+    """
+    array_shape = the_5d_input.shape
+    return numpy.amin(the_5d_input.reshape(array_shape[0],
+                                           array_shape[1] / downscale_factor,
+                                           downscale_factor,
+                                           array_shape[2],
+                                           array_shape[3] / downscale_factor,
+                                           downscale_factor,
+                                           array_shape[4] / downscale_factor,
+                                           downscale_factor),
+                      axis=(2, 5, 7))
+
+
+def max_pool_3d(the_5d_input, downscale_factor):
+    """
+    Max-pools a 3d layer (represented as a 5d BZCXY array) by the same downscale_factor in each dimension. Assumes that each of
+    the 3 spatial dimensions has size divisible by the downscale factor.
+    """
+    array_shape = the_5d_input.shape
+    return numpy.amax(the_5d_input.reshape(array_shape[0],
+                                           array_shape[1] / downscale_factor,
+                                           downscale_factor,
+                                           array_shape[2],
+                                           array_shape[3] / downscale_factor,
+                                           downscale_factor,
+                                           array_shape[4] / downscale_factor,
+                                           downscale_factor),
+                      axis=(2, 5, 7))
