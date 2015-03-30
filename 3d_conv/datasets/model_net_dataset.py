fixed off by one error

3d_conv/datasets/model_net_dataset.py
@@ -187,7 +187,7 @@ class ModelNetIteratorClassifier(ModelNetIterator):
 
     def next(self, categories):
 
-        batch_indices = np.random.random_integers(0, self.dataset.get_num_examples(), self.batch_size)
+        batch_indices = np.random.random_integers(0, self.dataset.get_num_examples()-1, self.batch_size)
 
 
         patch_size = self.dataset.patch_size
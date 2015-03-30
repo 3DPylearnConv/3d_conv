2c6687449e82c6c12d5c32058a4c7fb2c2c8e712
chaddech <johndoe@example.com>
2015-03-30 18:18:34 -0400

    fixed off by one error

--- 3d_conv/datasets/model_net_dataset.py
+++ 3d_conv/datasets/model_net_dataset.py
@@ -187,7 +187,7 @@ class ModelNetIteratorClassifier(ModelNetIterator):
 
     def next(self, categories):
 
-        batch_indices = np.random.random_integers(0, self.dataset.get_num_examples(), self.batch_size)
+        batch_indices = np.random.random_integers(0, self.dataset.get_num_examples()-1, self.batch_size)
 
 
         patch_size = self.dataset.patch_size
