e0a368f51d24b00589aa18d5445c8fba441110f7
chaddech <johndoe@example.com>
2015-04-07 16:19:59 -0400

    removed unnecessary code, added imports

--- 3d_conv/completion.py
+++ 3d_conv/completion.py
@@ -21,6 +21,11 @@ from theano.tensor.nnet.conv3d2d import *
 
 from logistic_sgd import LogisticRegression
 from datasets.model_net_dataset import ModelNetDataset
+from visualization.visualize import *
+
+from layers.hidden_layer import *
+from layers.conv_layer_3d import *
+from layers.layer_utils import *
 
 
 from matplotlib import pyplot as plt
@@ -119,153 +124,6 @@ class reconLayer(object):
 
     def return_output(self):
         return self.output
-
-
-
-class HiddenLayer(object):
-    def __init__(self, rng, input, n_in, n_out, drop, W=None, b=None,
-                 activation=T.tanh, p=0.5):
-        """
-        Typical hidden layer of a MLP: units are fully-connected and have
-        sigmoidal activation function. Weight matrix W is of shape (n_in,n_out)
-        and the bias vector b is of shape (n_out,).
-
-        NOTE : The nonlinearity used here is tanh
-
-        Hidden unit activation is given by: tanh(dot(input,W) + b)
-
-        :type rng: numpy.random.RandomState
-        :param rng: a random number generator used to initialize weights
-
-        :type input: theano.tensor.dmatrix
-        :param input: a symbolic tensor of shape (n_examples, n_in)
-
-        :type n_in: int
-        :param n_in: dimensionality of input
-
-        :type n_out: int
-        :param n_out: number of hidden units
-
-        :type activation: theano.Op or function
-        :param activation: Non linearity to be applied in the hidden
-                           layer
-        """
-        self.input = input
-        # end-snippet-1
-
-        # `W` is initialized with `W_values` which is uniformely sampled
-        # from sqrt(-6./(n_in+n_hidden)) and sqrt(6./(n_in+n_hidden))
-        # for tanh activation function
-        # the output of uniform if converted using asarray to dtype
-        # theano.config.floatX so that the code is runable on GPU
-        # Note : optimal initialization of weights is dependent on the
-        #        activation function used (among other things).
-        #        For example, results presented in [Xavier10] suggest that you
-        #        should use 4 times larger initial weights for sigmoid
-        #        compared to tanh
-        #        We have no info for other function, so we use the same as
-        #        tanh.
-        if W is None:
-            W_values = numpy.asarray(numpy.random.normal(loc=0., scale=.01, size=(n_in, n_out)), dtype=theano.config.floatX)
-
-            if activation == theano.tensor.nnet.sigmoid:
-                W_values *= 4
-
-            W = theano.shared(value=W_values, name='W', borrow=True)
-
-        if b is None:
-            b_values = numpy.ones((n_out,), dtype=theano.config.floatX)
-            b = theano.shared(value=b_values, name='b', borrow=True)
-
-        self.W = W
-        self.b = b
-
-        lin_output = T.dot(input, self.W) + self.b
-        output = activation(lin_output)
-        droppedOutput = dropout(rng, output, p)
-
-        self.output = T.switch(T.neq(drop, 0), droppedOutput, output)
-
-        # parameters of the model
-        self.params = [self.W, self.b]
-
-
-class ConvLayer3D(object):
-    """3D Layer of a convolutional network """
-
-    def __init__(self, rng, input, filter_shape, image_shape, drop, poolsize=(2, 2), p=0.5):
-        """
-        Allocate a layer with shared variable internal parameters.
-
-        :type rng: numpy.random.RandomState
-        :param rng: a random number generator used to initialize weights
-
-        :type input: theano.tensor.dtensor4
-        :param input: symbolic image tensor, of shape image_shape
-
-        :type filter_shape: tuple or list of length 4
-        :param filter_shape: (number of filters, num input feature maps,
-                              filter height, filter width)
-
-        :type image_shape: tuple or list of length 4
-        :param image_shape: (batch size, num input feature maps,
-                             image height, image width)
-
-        :type poolsize: tuple or list of length 2
-        :param poolsize: the downsampling (pooling) factor (#rows, #cols)
-        """
-
-        #assert image_shape[1] == filter_shape[1]
-        self.input = input
-
-        # there are "num input feature maps * filter height * filter width"
-        # inputs to each hidden unit
-        fan_in = numpy.prod(filter_shape[1:])
-        # each unit in the lower layer receives a gradient from:
-        # "num output feature maps * filter height * filter width" /
-        #   pooling size
-        fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) /
-                   numpy.prod(poolsize))
-        # initialize weights with random weights
-        self.W = theano.shared(numpy.asarray(numpy.random.normal(loc=0., scale=.01, size=filter_shape), dtype=theano.config.floatX),
-            borrow=True)
-
-        # the bias is a 1D tensor -- one bias per output feature map
-        b_values = numpy.ones((filter_shape[0],), dtype=theano.config.floatX)
-        self.b = theano.shared(value=b_values, borrow=True)
-
-        # convolve input feature maps with filters
-        conv_out = conv3d(
-            signals=input,
-            filters=self.W,
-            signals_shape=image_shape,
-            filters_shape=filter_shape,
-            border_mode='valid'
-
-        )
-
-
-        """
-        # downsample each feature map individually, using maxpooling
-        pooled_out = downsample.max_pool_2d(
-            input=conv_out,
-            ds=poolsize,
-            ignore_border=True
-        )
-        """
-        # add the bias term. Since the bias is a vector (1D array), we first
-        # reshape it to a tensor of shape (1, n_filters, 1, 1). Each bias will
-        # thus be broadcasted across mini-batches and feature map
-        # width & height
-        out = relu(conv_out + self.b.dimshuffle('x', 'x', 0, 'x', 'x'))
-
-
-        droppedOutput = dropout(rng, out, p)
-
-        self.output = T.switch(T.neq(drop, 0), droppedOutput, out)
-
-        # store parameters of this layer
-        self.params = [self.W, self.b]
 
 
 def evaluate(learning_rate=0.001, n_epochs=200,
