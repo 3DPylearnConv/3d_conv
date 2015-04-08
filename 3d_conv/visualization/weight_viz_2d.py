b35b33a8fa26e2fa9340a362934d3e3f2e0a78b9
jvarley <jakevarley@gmail.com>
2015-04-08 17:14:34 -0400

    fixed 2d weight vizualization

--- 3d_conv/visualization/weight_viz_2d.py
+++ 3d_conv/visualization/weight_viz_2d.py
@@ -7,13 +7,13 @@ from pylearn2.gui.patch_viewer import make_viewer
 #to show, may need to run:
 #export PYLEARN2_VIEWER_COMMAND=eog
 
-# weights of shape (out channel, row, column, time ,in channel)
+# weights of shape (out channel, time, in channel, row, column)
 def gen_weight_patches(weights, save=False):
 
     #weights = np.random.rand(20, 3, 3, 1)
     s0, s1, s2, s3, s4 = weights.shape
-    weights = weights.reshape(s0*s1, s2, s3, s4)
-    weights = scipy.ndimage.zoom(weights, [1, 2, 2, 1], order=0)
+    weights = weights.reshape(s0*s1*s2, s3, s4, 1)
+    weights = scipy.ndimage.zoom(weights, [1, 4, 4, 1], order=0)
     viewer = make_viewer(weights)
 
     if save:
