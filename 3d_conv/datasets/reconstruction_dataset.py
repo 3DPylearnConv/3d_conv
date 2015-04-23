fixed scaling from inches to meters

3d_conv/datasets/reconstruction_dataset.py
@@ -158,7 +158,8 @@ def build_training_example(binvox_file_path, model_pose_filepath, single_view_po
     non_zero_arr[3] = 1.0
 
     translate_arr = np.array(translate).reshape(3, 1)
-    scale /=2.7
+    #inches to meters
+    scale /=2.54
 
     non_zero_arr[0:3, :] = non_zero_arr[0:3, :] + translate_arr * 100 / (scale)
 
@@ -178,6 +179,10 @@ def build_training_example(binvox_file_path, model_pose_filepath, single_view_po
     max_z = pc2_out[2, :].max()
 
     center = (min_x + (max_x-min_x)/2.0, min_y + (max_y-min_y)/2.0, min_z + (max_z-min_z)/2.0)
+
+    viz.visualize_pointclouds(pc2_out.T, non_zero_arr1.T[:, 0:3], False, True)
+    import IPython
+    IPython.embed()
 
     #now non_zero_arr and pc points are in the same frame of reference.
     #since the images were captured with the model at the origin
@@ -231,9 +236,6 @@ class ReconstructionIterator(collections.Iterator):
             # viz.visualize_3d(x)
             # viz.visualize_3d(y)
             # viz.visualize_pointcloud(pc2_out[0:3, :].T)
-            # viz.visualize_pointclouds(pc2_out.T, non_zero_arr1.T[:, 0:3], False, True)
-            # import IPython
-            # IPython.embed()
 
             batch_y[i, :, :, :, :] = y
             batch_x[i, :, :, :, :] = x